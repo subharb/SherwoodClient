c0ddf33a0ffb42e9a15d64c9fb63c462eda096a3
Subharb <david@wasabiappfactory.com>
2020-11-13 15:29:14 +0000

    corregido provider

--- src/providerSherwood.js
+++ src/providerSherwood.js
@@ -6,26 +6,47 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { ThemeProvider }  from 'styled-components';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+const theme = {
+    buttonContinue : {
+        background : "#48bb78",
+        color : "#FFF"
+    },
+    buttonCancel: {
+        background : "#e53e3e",
+        color : "#FFF"
+    },
+    error : {
+        color : "#e53e3e",
+    },
+    edit : {
+        color : "#81e6d9",
+    }
+
+}
 
 export default class ProviderSherwood extends Component {
     render() {
         return (
             <Provider store={store}>
-                <LocalizeProvider initialize={{
-                    languages: [
-                    { name: "English", code: "en" },
-                    { name: "Spanish", code: "es" }
-                    ],
-                    translation: globalTranslations,
-                    options: {
-                    defaultLanguage: "en",
-                        renderToStaticMarkup: renderToStaticMarkup
-                    }
-                }}>
-                    {this.props.children}
-                </LocalizeProvider>
+                <ThemeProvider theme={theme}>
+                    <LocalizeProvider initialize={{
+                        languages: [
+                        { name: "English", code: "en" },
+                        { name: "Spanish", code: "es" }
+                        ],
+                        translation: globalTranslations,
+                        options: {
+                        defaultLanguage: "en",
+                            renderToStaticMarkup: renderToStaticMarkup
+                        }
+                    }}>
+                        {this.props.children}
+                    </LocalizeProvider>
+                </ThemeProvider>
             </Provider>
         )
     }
