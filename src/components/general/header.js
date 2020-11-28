6e14a480bd67774d3f563813c838f58abb3734dd
Subharb <david@wasabiappfactory.com>
2020-11-28 17:47:44 +0000

    Añadido bulma via css Ajustado header con estilos de bulma

--- src/components/general/header.js
+++ src/components/general/header.js
@@ -29,7 +29,59 @@ const LogoHolder = styled.div`
 `;
 
 export default class Header extends Component {
-    render() {
+    render(){
+        return (
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <Link class="navbar-item" to="/">
+                        <img src={pathLogo} alt="Sherwood Science" height="60" />
+                        Sherwood Science
+                    </Link>
+
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                 
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            Patients
+                        </a>
+
+                        <div class="navbar-dropdown">
+                            <Link class="navbar-item" to="/patient/register">
+                                Register
+                            </Link>
+                            <Link class="navbar-item" to="/patiend/login">
+                                Login
+                            </Link>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                        <Link class="button is-primary" to="/researcher/register">
+                            <strong>Sign up</strong>
+                        </Link>
+                        <Link class="button is-light" to="/researcher/login">
+                            Log in
+                        </Link>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </nav>
+        )
+    }
+    /* render() {
         return (
             <nav>
                 <HeaderContainer className="">
@@ -49,7 +101,7 @@ export default class Header extends Component {
                 </HeaderContainer>
             </nav>
         )
-    }
+    } */
 }
 
 Header.propTypes = {
