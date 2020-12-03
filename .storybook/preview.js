85df6670e287a0ec2658b362829428ae7f8fe0ed
Subharb <david@wasabiappfactory.com>
2020-12-03 17:54:03 +0000

    creado nuevas stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,4 +3,4 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
-localStorage.setItem("jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiN2Q4NDA1NjktNjA5Mi00ZGI5LWI0NDgtZjlhMWYxNzVmZTRmIiwiaWF0IjoxNjA2ODI1OTc0LCJleHAiOjE2MDY4Mjk1NzR9.nPAI1XE6hzBW8nkfZbom7BO7hKRRw1SDa4c0qf_Me88")
+localStorage.setItem("jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiN2Q4NDA1NjktNjA5Mi00ZGI5LWI0NDgtZjlhMWYxNzVmZTRmIiwiaWF0IjoxNjA3MDAwMDM3LCJleHAiOjE2MDcwMDM2Mzd9.OZbwPW2PDFMaKMXcorcyWLO7waUHeiH764eIVkzBbZo")
---
+++ src/stories/InvestigationModules/Survey.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Survey from '../../components/investigation/fill/survey_form';
+import ProviderSherwood from '../../providerSherwood';
+import { edc_data1 } from '../example_data';
+
+export default {
+    title: 'Investigation/Modules/Survey',
+    component: Survey, 
+    parameters: { actions: { argTypesRegex: '^callBack.*' } },
+    argTypes: {
+        initialData: {
+            control: "object"
+        }
+},
+  decorators: [story => 
+                <ProviderSherwood>
+                        {story()}
+                </ProviderSherwood>],
+};
+
+const Template = (args) => <Survey {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+    initialData : edc_data1(),
+    callBackData : (values) => {console.log("Callback BasicInfo", JSON.stringify(values));alert(values)} 
+};
+
+ 
