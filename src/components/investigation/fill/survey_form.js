7bf80e4a4ac05fa10483f51a4aaf9ce80689e2dc
Subharb <david@wasabiappfactory.com>
2020-12-04 10:28:14 +0000

    survey form no usa Form

--- src/components/investigation/fill/survey_form.js
+++ src/components/investigation/fill/survey_form.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
-import Form from '../../general/form';
+import { validateField } from '../../../utils/index';
 import { templateField } from '../../../utils';
 import SuccessComponent from '../../general/success_component';
 import Modal from '../../general/modal';
+import { ButtonContinue } from '../../general/mini_components';
+import { Field, reduxForm } from 'redux-form'
+import FieldSherwood from '../../general/FieldSherwood';
+import { Translate } from 'react-localize-redux';
 
 /**
  * SurveyForm se encarga de mostrar los formularios de cada sección, los valida y si son correctos avisa
@@ -44,15 +48,76 @@ class SurveyForm extends Component {
         this.props.history.push("/investigation/show/"+this.props.uuidInvestigation);
     }
     render() {
-        return this.props.initialData.sections.map(section => {
-            console.log(section.name);
-            return (
-                <div>
-                    <h5>{section.name}</h5>
-                    <Form fields={section.fields} answeringMode callBackForm={this.saveSurvey} />
-                </div>)
-        });
-        
+        return (
+            <div className="container">
+                <form data-testid="form" className="form-group" onSubmit={this.props.handleSubmit(values => {this.props.callBackForm(values)})}  >
+                    {
+                        this.props.initialData.sections.map(section => {
+                            console.log(section.name);
+                            return ([
+                                <div className="row" style={{paddingTop:'1rem'}}>
+                                    <h5>{section.name}</h5>
+                                </div>,
+                                <div className="row">
+                                    {
+                                        section.fields.map(field => {
+                                            return(
+                                                <div style={{marginRight:'1rem'}}>
+                                                    <Field
+                                                        size = "s6"
+                                                        name={field.name}
+                                                        type={field.type}
+                                                        required={field.required}
+                                                        component={FieldSherwood}
+                                                        label={field.label}/>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                    
+                                </div>
+                            ]
+                            )
+                        })
+                    }
+                    <div className="row" style={{paddingTop:'1rem'}}>
+                        <ButtonContinue type="submit" data-testid={this.props.dataTestid} >
+                            <Translate id="investigation.create.save" />
+                        </ButtonContinue>
+                    </div>
+                </form>
+            </div>
+        )
     }
+            
 }
+
+function validate(values, props){
+    const errors = {};
+    props.initialData.sections.forEach(section => {
+        Object.keys(section.fields).forEach(key => {
+            const field = {...section.fields[key]}
+            console.log(key+" : "+field.validation+" "+values[key]);
+            //Se puede comparar con otro valor del form si existe el campo validationField o con un valor que se pasa en validationValue
+            const fieldValueCompare = field.validationField ? values[field.validationField] : field.validationValue ? props.translate(field.validationValue) : null;
+            const validation = validateField({  
+                                    value : values[key], 
+                                    validation:field.validation, 
+                                    required:field.required
+                                },
+                                fieldValueCompare);
+            if(!validation.result){
+                errors[key] = validation.messageCode;
+            }
+        })
+    })
+    //console.log(errors);
+    return errors;
+}
+
+SurveyForm = reduxForm({
+    validate,
+    form: 'form'  // a unique identifier for this form               
+  })(SurveyForm)
+
 export default withRouter(SurveyForm);
