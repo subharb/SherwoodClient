10e717978614f6f5dd97b78e6171136e2b841d7c
Subharb <david@wasabiappfactory.com>
2020-12-10 10:49:16 +0000

    verificar el ltoken no mandaba el token

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,5 +3,5 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
-localStorage.setItem("jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZjRkYzE4YTYtNWFlNC00ZmExLTk4MjUtYjQxNjc0NDViOGM3IiwiaWF0IjoxNjA3NTQwNzg4LCJleHAiOjE2MDc1NDQzODh9.fWp75xPHG8212bl6GBY1ESVF1bbYLLvVeKhvL1Zkytg");
+localStorage.setItem("jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZjRkYzE4YTYtNWFlNC00ZmExLTk4MjUtYjQxNjc0NDViOGM3IiwiaWF0IjoxNjA3NTgwODMxLCJleHAiOjE2MDc1ODQ0MzF9.HB0t2iHelBH3jEpPy5M_LKWUAVfpUvcPg_MsXoBdpX8");
 localStorage.setItem("type", "researcher");
