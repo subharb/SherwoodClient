3b03e5bb0466dda1b16f43b6548244d5c4b3a0a3
Subharb <david@wasabiappfactory.com>
2020-12-23 10:54:17 +0000

    pasamos todos los records, luego el componente filtra por los del paciente que se pasa por parametro

--- src/stories/InvestigationModules/Fill/ShowPatientRecords.stories.js
+++ src/stories/InvestigationModules/Fill/ShowPatientRecords.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ShowPatientRecords from '../../../components/investigation/show/fill/show_patient_records';
 import ProviderSherwood from '../../../providerSherwood';
-import { records_patient1, edc_data1 } from '../../example_data';
+import { records_patient1, edc_data1, records_patient2, patients_personal_data } from '../../example_data';
 
 export default {
     title: 'Investigation/Fill/ShowPatientRecords',
@@ -49,18 +49,13 @@ Table.args = {
     callBackForm : (values) => {console.log("Callback BasicInfo", JSON.stringify(values));alert(values)} 
 };
 export const Submissions = Template.bind({});
+
+const recordsPatients = [...records_patient1().records, ...records_patient2().records];
 Submissions.args = {
     
     uuidInvestigation:"238b181e-28fd-47a1-bae6-a973c08c8684",
-    patient:{
-        "id": "5fdcd77b98125b1f1f0e627d",
-        "personalData": {
-            "name": "John",
-            "surname": "Hopkins",
-            "email": "patient@sherwood.science",
-            "phone": "+34 545454"
-        }
-    },
+    patient:patients_personal_data()[0],
+    initialData:recordsPatients,
     survey:edc_data1().surveys[0],
     mode:"elements",
     callBackForm : (values) => {console.log("Callback BasicInfo", JSON.stringify(values));alert(values)} 
