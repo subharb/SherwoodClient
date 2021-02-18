2f017e03fbf368e136f5a4d2532254c4f6617e32
Subharb <david@wasabiappfactory.com>
2021-02-18 09:01:01 +0000

    Ajustado estilos en listpatients

--- src/pages/hospital/ListPatients.js
+++ src/pages/hospital/ListPatients.js
@@ -9,19 +9,19 @@ function ListPatients(props) {
     const { pathname }= useRouter(props.initialState ? props.initialState.pathname : false);
 
     return (
-        <BoxBckgr color="text.primary">
+        <BoxBckgr color="text.primary" style={{padding:"1rem"}}>
             <Grid container spacing={3}>
-                <Grid item xs={12} style={{textAlign:"center"}}>
+                <Grid item xs={12} style={{textAlign:"center", color:"white"}}>
                     <Typography variant="h4" gutterBottom display="inline">
                         {props.name} {props.surnames}
                     </Typography>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} style={{color:"white"}}>
                     <Typography variant="h4" gutterBottom display="inline">
                         Hospital Ward
                     </Typography>
                 </Grid>
-                <Grid item xs={12} container spacing={3}>
+                <Grid item xs={12} container spacing={3} justify="center">
                     {props.listPatients.map(item => {
                         if(item.dateIn === null){
                             return(
