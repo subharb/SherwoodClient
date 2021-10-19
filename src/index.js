bf58b4a187248db7e3ee7044a7ea9c278e16c4ac
Subharb <david@wasabiappfactory.com>
2021-10-19 05:56:23 +0100

    quitado hotjar

--- src/index.js
+++ src/index.js
@@ -13,25 +13,12 @@ import { Integrations } from "@sentry/tracing";
 
 if (process.env.NODE_ENV === 'production') {
     console.log = function () {};
-
 }
 
-Sentry.init({
-  dsn: "https://cf032c10a38f45309939fcf46fa7794f@o491166.ingest.sentry.io/5992674",
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN);
 // or with require() syntax:
 // const mixpanel = require('mixpanel-browser');
-
-
-
 
 ReactDOM.render(
     <ProviderSherwood>
