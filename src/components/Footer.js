da8e1e34415ed8568dd73319605cbfecf35d3963
Subharb <david@wasabiappfactory.com>
2022-03-03 06:20:17 +0000

    añadido hotjar añadido un placeholder para poder quitar campos cuando se han puesto todos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v2.5.2`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v2.5.3`}
               />
             </ListItem>
           </List>
