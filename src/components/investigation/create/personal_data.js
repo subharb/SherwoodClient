90a4d1213dbf716215e44bff134b90c91c03a6ee
Subharb <david@wasabiappfactory.com>
2022-03-06 17:29:59 +0000

    corregido maquetacion del titulo de home de test y de imagenes

--- src/components/investigation/create/personal_data.js
+++ src/components/investigation/create/personal_data.js
@@ -129,7 +129,7 @@ class PersonalData extends Component{
                             noFooter />
                     </Grid>
                     <Grid item xs={6}>
-                    <EnhancedTable droppableId="selectedFields" miniTable noSelectable noFooter
+                        <EnhancedTable droppableId="selectedFields" miniTable noSelectable noFooter
                             titleTable="Selected Personal Fields" rows={rowsSelected} headCells={headSelected} 
                             callBackCheckbox={(id, param, value) => this.changeCheckbox(id, param, value)} />
                     </Grid>
