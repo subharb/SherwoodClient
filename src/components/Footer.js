b2cbba298a2d325886847613feb5d96b3da56cce
David Shaikh <>
2022-04-20 19:04:51 +0100

    añadido numero 2.6 al footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v2.5.0`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v2.6.0`}
               />
             </ListItem>
           </List>
