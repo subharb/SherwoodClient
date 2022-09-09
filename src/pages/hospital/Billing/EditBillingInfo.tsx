b6d3ccef9b3ce6b77e879e0403f39d4ecd1e1175
David Shaikh <>
2022-09-09 18:02:15 +0100

    Añadido traducciones

--- src/pages/hospital/Billing/EditBillingInfo.tsx
+++ src/pages/hospital/Billing/EditBillingInfo.tsx
@@ -6,36 +6,36 @@ const BILLING_INFO_FORM = {
     "logo" : {
         required : true,
         type:"file_base64",
-        label:"investigation.create.sections.LOGO",
-        shortLabel: "investigation.table.name",
+        label:"hospital.billing.billing_info.logo",
+        shortLabel: "hospital.billing.billing_info.logo",
         validation : "textMin2"
     },
     "address" : {
         required : true,
         type:"text",
-        label:"investigation.create.sections.section",
-        shortLabel: "investigation.table.name",
+        label:"hospital.billing.billing_info.address",
+        shortLabel: "hospital.billing.billing_info.address",
         validation : "textMin2"
     },
     "email" : {
         required : true,
         type:"text",
-        label:"investigation.create.sections.section",
-        shortLabel: "investigation.table.name",
+        label:"hospital.billing.billing_info.email",
+        shortLabel: "hospital.billing.billing_info.email",
         validation : "textMin2"
     },
     "phone" : {
         required : true,
         type:"text",
-        label:"investigation.create.sections.section",
-        shortLabel: "investigation.table.name",
+        label:"hospital.billing.billing_info.phone",
+        shortLabel: "hospital.billing.billing_info.phone",
         validation : "textMin2"
     },
     "currency" : {
         required : true,
         type:"text",
-        label:"investigation.create.sections.section",
-        shortLabel: "investigation.table.name",
+        label:"hospital.billing.billing_info.currency",
+        shortLabel: "hospital.billing.billing_info.currency",
         validation : "textMin2"
     },
 }
