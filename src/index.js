70935d53185581b69940bca590e45cefeafda221
David Shaikh <>
2022-10-05 22:24:45 +0100

    añdido sentry

--- src/index.js
+++ src/index.js
@@ -13,8 +13,12 @@ import { Integrations } from "@sentry/tracing";
 let dsn = "https://4e273ea1b6e04f6cb8f483979eb3233a@o491166.ingest.sentry.io/5992676";
 if (process.env.NODE_ENV === 'production') {
     console.log = function () {};
+    dsn = "https://cf032c10a38f45309939fcf46fa7794f@o491166.ingest.sentry.io/5992674"
 }
 
+Sentry.init({
+    dsn: dsn,
+  });
 
 mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN);
 // or with require() syntax:
