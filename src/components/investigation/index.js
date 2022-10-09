2ce37e2b355630d461cf6ce4b0117f3690cc2fa7
David Shaikh <>
2022-10-09 18:12:24 +0100

    Deshabilitar el mostrar investigaciones

--- src/components/investigation/index.js
+++ src/components/investigation/index.js
@@ -6,12 +6,12 @@ import { useParams
 
 export default function Investigation(props) {
     let { uuid } = useParams();
-    //return <UpdateInvestigation uuid={uuid} {...props} />
-    if(props.status === 0){
-        return <UpdateInvestigation uuid={uuid} {...props} />
-    }
-    else{
-        return <ShowInvestigation uuid={uuid} {...props} />
-    }
+    return <UpdateInvestigation uuid={uuid} {...props} />
+    // if(props.status === 0){
+    //     return <UpdateInvestigation uuid={uuid} {...props} />
+    // }
+    // else{
+    //     return <ShowInvestigation uuid={uuid} {...props} />
+    // }
 
 }
