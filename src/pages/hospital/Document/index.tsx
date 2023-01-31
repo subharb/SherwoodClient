e1085817e1275053fc4d9ef495d2bf15776c72bc
David Shaikh <>
2023-01-31 10:29:43 +0000

    Cambiado codigo como era en la version anterior

--- src/pages/hospital/Document/index.tsx
+++ src/pages/hospital/Document/index.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react"
 
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
-import {autoTable} from 'jspdf-autotable';
 import { HeaderDocument } from "./header";
 import SaveIcon from '@material-ui/icons/Save';
 import { BillingInfo } from "../Billing/types";
@@ -38,19 +37,19 @@ export const Document:React.FC<Props> = (props) => {
             // window.open(data);
             // }
 
-            // const canvas = await html2canvas(element, {
-            //     useCORS: true
-            // });
-            // const data = canvas.toDataURL('image/png');
+            const canvas = await html2canvas(element, {
+                useCORS: true
+            });
+            const data = canvas.toDataURL('image/png');
 
-            // const pdf = new jsPDF();
-            // const imgProperties = pdf.getImageProperties(data);
-            // const pdfWidth = pdf.internal.pageSize.getWidth();
-            // const pdfHeight =
-            // (imgProperties.height * pdfWidth) / imgProperties.width;
+            const pdf = new jsPDF();
+            const imgProperties = pdf.getImageProperties(data);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight =
+            (imgProperties.height * pdfWidth) / imgProperties.width;
 
-            // pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
-            // pdf.save(props.name+'.pdf');
+            pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+            pdf.save(props.name+'.pdf');
 
             // const elemntToPrint = document.getElementById("print");
             // if(elemntToPrint){
@@ -58,57 +57,7 @@ export const Document:React.FC<Props> = (props) => {
             //     .then(function (dataUrl) {
             //         saveAs(dataUrl, 'my-node.png');
             //     });
-
-            
-            var doc = new jsPDF('p', 'mm', [720, 1018]);
-
-
-            // const head = [['ID', 'Country', 'Index', 'Capital']]
-            // const data = [
-            //     [1, 'Finland', 7.632, 'Helsinki'],
-            //     [2, 'Norway', 7.594, 'Oslo'],
-            //     [3, 'Denmark', 7.555, 'Copenhagen'],
-            //     [4, 'Iceland', 7.495, 'Reykjavík'],
-            //     [5, 'Switzerland', 7.487, 'Bern'],
-            //     [9, 'Sweden', 7.314, 'Stockholm'],
-            //     [73, 'Belarus', 5.483, 'Minsk'],
-            // ]
-        
-            // doc.autoTable(head, data, {
-            //     startY: doc.autoTable() + 70,
-            //     margin: { horizontal: 10 },
-            //     styles: { overflow: "linebreak" },
-            //     bodyStyles: { valign: "top" },
-            //     columnStyles: { email: { columnWidth: "wrap" } },
-            //     theme: "striped",
-            //     showHead: "everyPage"
-            //   });
-
-           // Convert HTML to PDF in JavaScript
-           doc.html(element, {
-                callback: function(doc) {
-                    const pageCount = doc.getNumberOfPages();
-                    for(let i = 1; i <= pageCount; i++) {
-                        doc.setPage(i);
-                        const pageSize = doc.internal.pageSize;
-                        const pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
-                        const pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
-                        const footer = `Page ${i} of ${pageCount}`;
-                        doc.text(footer, pageWidth / 2 - (doc.getTextWidth(footer) / 2), pageHeight - 15, { baseline: 'bottom' });
-
-                    }
-                    doc.save(props.name+".pdf");
-                },
-                x: 0,
-                y: 0,
-                margin: [100, 0, 100, 60],
-                autoPaging: 'text',
-                width: 720, //target width in the PDF document
-                windowWidth: 1000 //window width in CSS pixels
-            });
-
-
-
+                
             // }
             
         }
@@ -116,8 +65,6 @@ export const Document:React.FC<Props> = (props) => {
             console.log("No ref")
         }
     }
-
-      
     if(props.size === "A4"){
         return(
             <div style={{overflow:'scroll'}}>
@@ -134,7 +81,7 @@ export const Document:React.FC<Props> = (props) => {
                         {
                             props.children
                         }
-                        </Grid> 
+                        </Grid>   
                     </div>
                 </Grid>
             </div>
