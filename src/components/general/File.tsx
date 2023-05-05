71a7349a0b8c5ab75273303533e71ac877eb63a5
David Shaikh <>
2023-05-05 22:09:32 +0100

    Corregido el subir imagenes

--- src/components/general/File.tsx
+++ src/components/general/File.tsx
@@ -256,7 +256,7 @@ const File:React.FC<Props> = (props) => {
     }, [filesSelected]);
     useEffect(() =>{
         async function loadFiles(){
-            if(typeof prevValue === "undefined" && props.value && props.value.length > 0 ){
+            if(typeof prevValue === "undefined" && props.value && props.value.length > 0 && props.mode === "show" ){
                 const tempFiles:FileUpload[] = props.value.map(file => {
                     return {
                         remoteName:file.file,
