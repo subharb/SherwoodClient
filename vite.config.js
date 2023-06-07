4063bf048d18c8eaa553e8d53075e9df9aa3c555
David Shaikh <>
2023-06-07 16:58:59 +0100

    CAmbiado global

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig(() => {
     define: {
         // By default, Vite doesn't include shims for NodeJS/
         // necessary for segment analytics lib to work
-        global: {},
+        global: '({})',
         "process.env.TESS_ENV": process.env.ENV,
       },
   };
