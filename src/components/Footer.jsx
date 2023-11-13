4593b95ac84333a3f4145ae65e463eb931738ef7
David Shaikh <>
2023-11-13 20:21:00 +0000

    Tabs funcionando en fondo blanco

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.3.14`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.3.15`}
               />
             </ListItem>
           </List>
