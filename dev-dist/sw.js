1f52840f9cf8797e12eae1b0e9c3b85e37e4b675
David Shaikh <>
2023-12-21 12:10:53 +0000

    actualizado paquetes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kk8idunpsr8"
+    "revision": "0.qmmvh5ibu6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
