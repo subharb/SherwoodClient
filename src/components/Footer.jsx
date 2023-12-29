914a27d99b57255259611ca33114ec8074950889
David Shaikh <>
2023-12-29 20:31:07 +0100

    Preparado para traducir sourcemaps

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.3.15`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.4.0`}
               />
             </ListItem>
           </List>
