4d1c73a81891c3e19e28415c5ec53acfc9a87d04
David Shaikh <>
2024-01-15 10:06:54 +0100

    Creado un selector de formularios que son previos a los fromularios de tabla, que muestran los submissions todos juntos en una tabla

---
+++ src/pages/hospital/patient/SubmissionsTableSelector.tsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { IField, IPatient, ISection, ISubmission, ISurvey } from '../../../constants/types';
+import SubmissionsTable from './SubmissionsTable';
+import { BillingInfo } from '../Billing/types';
+import { Button, Grid, Typography } from '@mui/material';
+import { Translate } from 'react-localize-redux';
+import SurveyRecordsTable from '../SurveyRecordsTable';
+import { ButtonBack } from '../../../components/general/mini_components';
+
+
+interface SubmissionsTableSelectorProps {
+    surveys:ISurvey[],
+    locale:string,
+    records: ISubmission[],
+    billingInfo: BillingInfo | null,
+    onSelectSubmission: (index:number) => void;
+}
+
+const SubmissionsTableSelector: React.FC<SubmissionsTableSelectorProps> = ({ surveys, locale, 
+                                        records, billingInfo, onSelectSubmission }) => {
+    const surveysWithRecords = useMemo(() => {
+        return records.reduce((acc:ISurvey[], current:ISubmission) => {
+                const surveyRecord = surveys.find((survey) => {
+                    return survey.uuid === current.uuidSurvey
+                })
+                if(surveyRecord && !acc.find((survey) => survey.uuid === surveyRecord.uuid)){
+                    acc.push(surveyRecord)
+                }
+                return acc;
+            }, [])                           
+    }, [records])                  
+    
+    const [selectedSurvey, setSelectedSurvey] = useState<ISurvey | null>(null);                                                                
+    
+    useEffect(() => {
+        if(surveysWithRecords.length === 1){
+            setSelectedSurvey(surveys[0]);
+        }
+    }, [surveysWithRecords]);
+
+    function onSurveySelected(survey:ISurvey){
+        setSelectedSurvey(survey);
+    }
+
+    function renderSelector(){
+        
+        return(
+            <Grid container xs={12}>
+                <Grid item xs={12}>
+                    <Typography variant='body2'>
+                        <Translate id="hospital.patient.submission-table.selector.title" />
+                    </Typography>
+                </Grid>
+                {
+                    surveysWithRecords.map((survey) => {
+                        return (
+                            <Grid item xs={12}>
+                                <Button onClick={() => onSurveySelected(survey)}>
+                                    <Typography variant='body2'>
+                                        {survey.name}
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        ) 
+    }                                                                
+    if(selectedSurvey){
+        const surveysRecords = records.filter((record) => record.uuidSurvey === selectedSurvey.uuid)
+        if(surveysRecords.length > 0){
+            const fieldsSurvey:IField[] = selectedSurvey.sections.reduce((acc:IField[], current:ISection) => {
+                return acc.concat(current.fields)
+            }, [])
+            return (
+                <Grid container xs={12}>
+                    {
+                        surveysWithRecords.length > 1 && 
+                        <Grid item xs={12}>
+                            <ButtonBack onClick={() => setSelectedSurvey(null)}></ButtonBack>
+                        </Grid>
+                    }
+                    
+                    <Grid item xs={12}>
+                        <SurveyRecordsTable fields={fieldsSurvey} submissions={records} 
+                        locale={locale}
+                        onSelectSubmission={(index) => onSelectSubmission(index)}  />
+                    </Grid>
+                </Grid>
+            )
+        }
+        else{
+            return <span>No records found</span>;
+        }
+        
+    }
+    return renderSelector()
+};
+
+export default SubmissionsTableSelector;
--- src/pages/hospital/patient/index.jsx
+++ src/pages/hospital/patient/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useMemo } from 'react'
 import { connect } from 'react-redux';
-import * as types from "../../../constants";
+
 import { Grid, Typography, Snackbar } from '@mui/material';
 import { EnhancedTable } from '../../../components/general/EnhancedTable';
 import { fetchSubmissionsPatientInvestigationAction, resetPatientsSubmissionsError } from '../../../redux/actions/submissionsPatientActions';
@@ -427,16 +427,17 @@ function Patient(props) {
                     </TypographyStyled>
         }
         else{
-            let fieldsSurvey;
-            if(currentSurveys.length > 0 && currentSurveys[0].type === types.TYPE_NURSE){
-                fieldsSurvey = currentSurveys[0].sections.reduce((acc, current) => {
-                    return acc.concat(current.fields)
-                }, [])
-            }
+            
+            return (
+                <SubmissionsTable typeSurveySelected={typeSurveySelected}
+                    surveys={currentSurveys} billingInfo={billingInfo} patient={patient}
+                    category={currentSurveys[0].category} records={filteredRecords}
+                    onSelectSubmission={(index) => selectSubmission(index)} />
+            )
             return (
                 <SubmissionsTable fieldsSurvey={fieldsSurvey} typeSurveySelected={typeSurveySelected}
                     surveys={currentSurveys} billingInfo={billingInfo} patient={patient}
-                    category={currentSurveys[0].category} filteredRecords={filteredRecords}
+                    category={currentSurveys[0].category} records={filteredRecords}
                     onSelectSubmission={(index) => selectSubmission(index)} />
             );
         }
