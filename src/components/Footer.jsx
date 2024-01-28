1ac3c4eef17709545ecee1ba4ff47b4a1a22f73f
David Shaikh <>
2024-01-28 19:39:38 +0000

    Para compilar research que da error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.4.3`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.5.0`}
               />
             </ListItem>
           </List>
