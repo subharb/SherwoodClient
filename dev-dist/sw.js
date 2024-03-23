be11d5a8658d5cb4f6e722bd21751a99c529069c
David Shaikh <>
2024-03-23 08:50:30 +0000

    CAmbiado version

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.350m434d3no"
+    "revision": "0.ub5gm0pjj1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
