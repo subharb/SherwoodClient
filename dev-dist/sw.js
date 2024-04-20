c58b18f160de3b4784857d5955d79f91b51dc7bc
David Shaikh <>
2024-04-20 08:51:38 +0100

    ACtualizado storybook

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kum3usl3alo"
+    "revision": "0.4e6emhftm6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
