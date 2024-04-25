122f4b2939f017580f871714ed4e4435a2276ce1
David Shaikh <>
2024-04-25 20:30:05 +0100

    Cambios en el viewer

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6e3354dc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q337rgfjij8"
+    "revision": "0.cprc3gu4t5g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
