2050a30b315817a4b0db8f6cd7de846b17d77bf0
David Shaikh <>
2024-04-26 15:53:07 +0100

    Eliminado unos imports

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t2gbqb7772g"
+    "revision": "0.5spscm9nrrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
