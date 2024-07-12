745720d8a1d6bc1b69f7a8450feb3294c7a88fbe
Aakanksha Gaikwad <aakanksha@sherwood.science>
2024-07-12 12:07:39 +0530

    cloudlfare site key added env

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
+define(['./workbox-6e3354dc'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.slj46lnro18"
+    "revision": "0.c1bo6tti0u"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
