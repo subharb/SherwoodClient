d66034158c94157dfeeb452dabdd72092a25fc1a
David Shaikh <>
2024-08-01 08:02:47 +0200

    Updated version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.6.2(BETA)`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.7.0`}
               />
             </ListItem>
           </List>
