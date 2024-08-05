61189437d535818988273c47cb3d9842428c727a
David Shaikh <>
2024-08-05 08:59:44 +0200

    Prevents consecutive calls to profileService

--- src/redux/actions/profileActions.js
+++ src/redux/actions/profileActions.js
@@ -4,18 +4,24 @@ import { fetchProfileService } from "../../services";
 
 export function fetchProfileInfoAction(uuidInvestigation) {
   
-  return async (dispatch) => {
-    dispatch({ type: types.FETCH_PROFILE_LOADING });
-    return fetchProfileService(uuidInvestigation)
+    return async (dispatch, getState) => {
+        const loadingState = getState().profile.loading;
+        
+        if (loadingState) {
+            console.log('Profile fetch already in progress');
+            return;
+        }
+        dispatch({ type: types.FETCH_PROFILE_LOADING })
+        return fetchProfileService(uuidInvestigation)
         .then((response) => {
-            dispatch({
-            type: types.FETCH_PROFILE_SUCCESS,
-            profile: response.profileInfo,
-            });
-        })
+                dispatch({
+                type: types.FETCH_PROFILE_SUCCESS,
+                profile: response.profileInfo,
+                });
+            })
         .catch((error) => {
-            dispatch({ type: types.FETCH_PROFILE_ERROR });
-            throw error;
+                dispatch({ type: types.FETCH_PROFILE_ERROR });
+                throw error;
       });
   };
 }
