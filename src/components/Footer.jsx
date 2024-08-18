cb9f19f0711134dde646cf0eb3275d746a518a3c
David Shaikh <>
2024-08-18 21:07:03 +0200

    Añadido user roles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ function Footer() {
           <List>
             <ListItem button={true}>
               <ListItemText
-                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.7.2`}
+                primary={`© ${new Date().getFullYear()} - Sherwood Hospital v3.7.3`}
               />
             </ListItem>
           </List>
